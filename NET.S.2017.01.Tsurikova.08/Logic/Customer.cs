6216a310e006b04ba75ca045ea9852df00729cce
user <kionopia@gmail.com>
2017-07-15 14:08:53 +0300

    customer and its formattings added

---
+++ NET.S.2017.01.Tsurikova.08/Logic/Customer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// class for working with customer info
+    /// </summary>
+    public class Customer : IFormattable
+    {
+        public string Name { get; }
+        public string ContactPhone { get; }
+        public decimal Revenue { get; }
+
+        public Customer(string name, string phone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = phone;
+            Revenue = revenue;
+        }
+
+        /// <summary>
+        /// represents customer in a general format and current culture
+        /// </summary>
+        /// <returns>string repersentation</returns>
+        public override string ToString() => ToString("G", CultureInfo.CurrentCulture);
+
+        /// <summary>
+        /// represents customer in corresponding format and current culture
+        /// </summary>
+        /// <exception cref="FormatException">throws when string format is invalid</exception>
+        /// <param name="format">string format</param>
+        /// <returns>string representation</returns>
+        public string ToString(string format) => ToString(format, CultureInfo.CurrentCulture);
+
+        /// <summary>
+        /// represents customer in corresponding format and culture
+        /// </summary>
+        /// <exception cref="FormatException">throws when string format is invalid</exception>
+        /// <param name="format">string format</param>
+        /// <param name="formatProvider">format provider</param>
+        /// <returns>string representation</returns>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+                format = "G";
+            if (ReferenceEquals(formatProvider, null))
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "A":
+                    return $"{Name}, {Revenue.ToString("N2", formatProvider)}, {ContactPhone}";
+                case "P":
+                    return $"{ContactPhone}";
+                case "N":
+                    return $"{Name}";
+                case "R":
+                    return $"{Revenue.ToString("N2", formatProvider)}";
+                case "NR":
+                    return $"{Name}, {Revenue.ToString("N2", formatProvider)}";
+                default:
+                    throw new FormatException($"The {format} format string is not supported.");
+            }
+        }
+    }
+}
---
+++ NET.S.2017.01.Tsurikova.08/Logic/CustomerFormat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// class provides additional formatting options for customer
+    /// </summary>
+    public class CustomerFormat : IFormatProvider, ICustomFormatter
+    {
+        private readonly IFormatProvider parentProvider;
+
+        public CustomerFormat() : this(CultureInfo.CurrentCulture) { }
+        public CustomerFormat(IFormatProvider parent)
+        {
+            parentProvider = parent;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter)) return this;
+            return parentProvider.GetFormat(formatType);
+        }
+
+        /// <summary>
+        /// represents customer in corresponding format and culture
+        /// </summary>
+        /// <param name="format">string format</param>
+        /// <param name="arg">customer to be formatted</param>
+        /// <param name="formatProvider">format provider</param>
+        /// <exception cref="FormatException">throws when string format is invalid</exception>
+        /// <exception cref="ArgumentException">throws when arg isn't customer</exception>
+        /// <returns>string representation</returns>
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (!(arg is Customer)) throw new ArgumentException($"{nameof(arg)} isn't a customer");
+            if (ReferenceEquals(formatProvider, null))
+                formatProvider = parentProvider;
+
+            Customer customer = (Customer)arg;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "C":
+                    return $"{customer.Revenue.ToString("C2", formatProvider)}";
+                case "NP":
+                    return $"Name: {customer.Name}\nPhone: {customer.ContactPhone}";
+                default:
+                    return ((IFormattable)arg).ToString(format, formatProvider);
+            }
+        }
+    }
+}
